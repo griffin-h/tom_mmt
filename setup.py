bd1e2a7bb9ef69d6d60a08c75bc17f79be8f268c
Manisha Shrestha <ms1228@truman.edu>
2024-10-07 11:43:06 -0700

    version updated to 0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tom-mmt',
-    version='0.1.3',
+    version='0.1.4',
     description='MMT Observatory module for the TOM Toolkit',
     long_description=long_description,
     long_description_content_type='text/markdown',
