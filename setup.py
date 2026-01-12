2e609e739eedeeaf76aaed62de2f469979509423
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-01-12 13:40:46 -0800

    increment version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tom-mmt',
-    version='0.1.5',
+    version='0.1.6',
     description='MMT Observatory module for the TOM Toolkit',
     long_description=long_description,
     long_description_content_type='text/markdown',
